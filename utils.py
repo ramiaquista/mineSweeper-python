ad2fd58749fb96462e3c8aac9c5f7fb8ac79905c
Ramiro Aquistapace <ramiaquista70@gmail.com>
2022-04-23 17:09:19 +0200

    Add frames for the layout, add settings with the width,height and grid sizes, add cell object class

---
+++ utils.py
@@ -0,0 +1,7 @@
+import settings
+
+def height_percentage(percetange):
+    return (settings.HEIGHT / 100) * percetange
+
+def width_percentage(percetange):
+    return (settings.WIDTH / 100) * percetange
